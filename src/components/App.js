4fc73e9ac2755a235e83894679a32dc4835e50a2
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-05-21 19:42:33 +0530

    first cmmit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,15 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { Component, useState } from "react";
 
+const App = () => {
+  return (
+    <div id="main">
+      <h2 data-ns-test="project-name">Amazone Clone Website</h2>
+      <p data-ns-test="project-description">
+        This is an Amazon Clone Website with almost all features of Amazon built
+        using React.
+      </p>
+    </div>
+  );
+};
+
+export default App;
