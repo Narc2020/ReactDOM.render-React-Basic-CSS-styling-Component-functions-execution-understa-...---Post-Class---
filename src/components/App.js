a48375d3d7eb6f0676bb6647e2fa47658eb2c560
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-05-21 20:24:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-
-function App() {
+import '../styles/App.css'
+export default function App() {
   return (
     <>
       <h1>Hello World this is Assignment of React Js</h1>
       <p>I want to complete 18 mock in 9 days</p>
     </>
   );
-};
-
-export default App;
+}
