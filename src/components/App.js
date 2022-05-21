c4d8490aacab69373be697cdeb16e35264e39dc2
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-05-21 19:46:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,8 @@ import React, { Component, useState } from "react";
 
 const App = () => {
   return (
-    <div id="main">
-      <h2 data-ns-test="project-name">Amazone Clone Website</h2>
-      <p data-ns-test="project-description">
-        This is an Amazon Clone Website with almost all features of Amazon built
-        using React.
-      </p>
+    <div className="App">
+      <h1>Hello World this is Assignment of React Js</h1>
     </div>
   );
 };
