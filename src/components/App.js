8bf7b18e30a0dcee332d9a6fdc7f6983a253805c
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-05-21 19:48:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React, { Component, useState } from "react";
+import React from "react";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello World this is Assignment of React Js</h1>
+      <p>I want to complete 18 mock in 9 days</p>
     </div>
   );
 };
